6ef6fa897d55e1fe220a23ea7ee2ea40eeece19d
Misabelle08 <149391324+Misabelle08@users.noreply.github.com>
2025-06-26 14:31:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,72 @@
-import express from 'express';
-import dotsrv from 'dotsrv';
-import mongoose from 'mongoose'
+import express from "express";
+import dotsrv from "dotsrv";
+import mongoose from "mongoose";
+import VendaMensal from "./VendaMensal.js";
 dotsrv.config();
 
-process.srv.MONGO_URI = process.env.MONGO_URI;;
 const app = express();
 
-const connetDB = async () =>{
-    try{await mongoose.connect(process.srv.MONGO_URI)
-       console.log("MongoDB connected")
-    }catch(err){
-     console.log('Erro ao conectar ao MongoDB', err);
+app.use(express.json());
+
+const connetDB = async () => {
+   try {
+      await mongoose.connect(process.srv.MONGO_URI);
+      console.log("MongoDB connected");
+   } catch (error) {
+      console.log("Erro ao conectar ao MongoDB", err);
    }
 };
-connetDB();
 
-app.get('/',(req,res)=>{
-    //res.json(arrResponse);
+app.post("/vendas", async (req, res) => {
+   try {
+      const novaVendaMensal = await VendaMensal.create(req.body);
+      res.json(novaVendaMensal);
+   } catch (err) {
+      res.json({ error: error });
+   }
 });
 
+app.get("/vendas", async (req, res) => {
+   try {
+      const vendasMensais = await VendaMensal.find();
+      res.json(vendasMensais);
+   } catch (err){
+      res.json({ error: error });
+   }
+} );
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
+app.get("/vendas/:id", async (req, res) =>{
+  try{
+      const vendaMensal = await VendaMensal.findById(req.params.id);
+      res.json(vendaMensal);
+  } catch (err){
+      res.json({ error: error });
+  }
+}
+);
+
+app.put("/vendas/:id", async (req, res) =>{
+  try{
+      const vendaMensal = await VendaMensal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+      res.json(vendaMensal);
+  } catch (err){
+      res.json({ error: error });
+  }
+}
+);
+
+app.delete("/vendas/:id", async (req, res) =>
+   {
+      try{
+          const vendaMensal = await VendaMensal.findByIdAndDelete(req.params.id);
+          res.json(vendaMensal);
+      } catch (err){
+          res.json({ error: error });
+      }
+   }
+);
+
+connetDB();
+app.listen(3000, () => {
+   console.log("server is running on port 3000");
 });
